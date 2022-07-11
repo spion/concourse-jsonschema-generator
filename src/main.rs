e7939a2b17b88336e09f8fee2dc5be620472567d
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2022-07-11 10:55:37 +0100

    extract serialize

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use itertools::Itertools;
-use peg::error::ParseError;
-use peg::str::LineCol;
-use schema::types::Schema;
+use clap::Parser;
 use std::collections::HashMap;
 use std::fs;
-
-use clap::Parser;
-
-use schema::serialize::prop_type_to_jsonschema;
 
 mod convert;
 mod lit;
 mod schema;
-
-fn parse(contents: &str) -> Result<Vec<Schema>, ParseError<LineCol>> {
-  let lit_document = lit::parse(&contents)?;
-  Result::Ok(convert::transform_to_jsonschemas(&lit_document))
-}
 
 /// Concourse documentation parser
 #[derive(Parser, Debug)]
@@ -30,72 +18,30 @@ struct Args {
   #[clap(short, long, default_value = "schema.json")]
   schema: String,
 }
+
 pub fn main() {
   let args = Args::parse();
 
-  // Typename mappings:
-  // config -> match everything
-  //
   let schema_docs = args
     .litfiles
     .iter()
-    .flat_map(|path| match parse(&fs::read_to_string(path).unwrap()) {
-      Ok(doc) => doc,
+    .flat_map(|path| {
+      let contents = fs::read_to_string(path).unwrap();
+      let lit_document = lit::parse(&contents);
+      match lit_document {
+        Ok(doc) => convert::transform_to_jsonschemas(&doc),
 
-      Err(e) => {
-        println!("In {}", path);
-        println!("{}", e);
-        panic!("Unexpected parse error, aborting");
+        Err(e) => {
+          println!("In {}", path);
+          println!("{}", e);
+          panic!("Unexpected parse error, aborting");
+        }
       }
     })
     .map(|schema| (schema.schema_name.clone(), schema))
     .collect::<HashMap<_, _>>();
 
-  let definitions = schema_docs
-    .iter()
-    .map(|(schema_name, schema)| {
-      let schema_props = schema
-        .properties
-        .iter()
-        .map(|(prop_name, prop)| {
-          format!(
-            r#""{}":{}"#,
-            prop_name,
-            prop_type_to_jsonschema(&prop.type_name, Some(&prop.docs))
-          )
-        })
-        .collect::<Vec<_>>();
-
-      let schema_obj = if schema_props.len() > 0 {
-        format!(
-          r#"{{"additionalProperties":false,"type":"object","properties":{{{}}}}}"#,
-          schema_props.join(",")
-        )
-      } else if schema_name == "number" {
-        r#"{"type":"number"}"#.to_string()
-      } else if schema_name == "boolean" {
-        r#"{"type":"boolean"}"#.to_string()
-      } else if schema_name == "config" || schema_name == "value" {
-        r#"{"type":"object","patternProperties":{".*":{"additionalProperties":true}}}"#.to_string()
-      } else {
-        r#"{"type":"string"}"#.to_string()
-      };
-
-      format!(r#""{}":{}"#, schema_name, schema_obj)
-    })
-    .join(",");
-
-  let schema = format!(
-    r###"
-    {{
-      "$schema": "http://json-schema.org/draft-04/schema#",
-      "$ref": "#/definitions/pipeline",
-      "additionalProperties": true,
-      "definitions":{{{}}}
-    }}
-    "###,
-    definitions
-  );
+  let schema = schema::serialize::serialize(&schema_docs);
 
   println!("{}", schema);
 }
--- src/schema/serialize.rs
+++ src/schema/serialize.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
 use crate::schema::types::*;
 use itertools::Itertools;
 
-pub fn prop_type_to_jsonschema_nodocs(prop_type: &PropertyType) -> String {
+fn prop_type_to_jsonschema_nodocs(prop_type: &PropertyType) -> String {
   prop_type_to_jsonschema(prop_type, None)
 }
 
-pub fn prop_type_to_jsonschema(prop_type: &PropertyType, description: Option<&String>) -> String {
+fn prop_type_to_jsonschema(prop_type: &PropertyType, description: Option<&String>) -> String {
   let additional = match prop_type {
     PropertyType::OneOf(types) => {
       let all_types_count = types.len();
@@ -47,4 +49,52 @@ pub fn prop_type_to_jsonschema(prop_type: &PropertyType, description: Option<&St
   };
 
   format!(r#"{{{}{}}}"#, desc, additional)
+}
+
+pub fn serialize(schema_docs: &HashMap<String, Schema>) -> String {
+  let definitions = schema_docs
+    .iter()
+    .map(|(schema_name, schema)| {
+      let schema_props = schema
+        .properties
+        .iter()
+        .map(|(prop_name, prop)| {
+          format!(
+            r#""{}":{}"#,
+            prop_name,
+            prop_type_to_jsonschema(&prop.type_name, Some(&prop.docs))
+          )
+        })
+        .collect::<Vec<_>>();
+
+      let schema_obj = if schema_props.len() > 0 {
+        format!(
+          r#"{{"additionalProperties":false,"type":"object","properties":{{{}}}}}"#,
+          schema_props.join(",")
+        )
+      } else if schema_name == "number" {
+        r#"{"type":"number"}"#.to_string()
+      } else if schema_name == "boolean" {
+        r#"{"type":"boolean"}"#.to_string()
+      } else if schema_name == "config" || schema_name == "value" {
+        r#"{"type":"object","patternProperties":{".*":{"additionalProperties":true}}}"#.to_string()
+      } else {
+        r#"{"type":"string"}"#.to_string()
+      };
+
+      format!(r#""{}":{}"#, schema_name, schema_obj)
+    })
+    .join(",");
+
+  format!(
+    r###"
+  {{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "$ref": "#/definitions/pipeline",
+    "additionalProperties": true,
+    "definitions":{{{}}}
+  }}
+  "###,
+    definitions
+  )
 }
